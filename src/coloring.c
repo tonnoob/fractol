67a69ef18657c8ad67432cd26b9f712d562908d4
Otton Sousa De Santana <osousa-d@c1r1p3.42sp.org.br>
2026-01-27 12:38:51 -0300

    debug argv

--- src/coloring.c
+++ src/coloring.c
@@ -6,26 +6,31 @@
 /*   By: osousa-d <osousa-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 07:03:33 by otton-sousa       #+#    #+#             */
-/*   Updated: 2026/01/27 12:06:31 by osousa-d         ###   ########.fr       */
+/*   Updated: 2026/01/27 12:27:15 by osousa-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
+#include <math.h>
 
 int get_color(int iter, int max_iter)
 {
-	int		color;
 	double	t;
 	int		r;
 	int		g;
 	int		b;
 
 	if (iter == max_iter)
-		return (0x0);
+		return (0xF5F2FF); // fundo claro onírico
+
 	t = (double)iter / (double)max_iter;
-	r = (int)(5 * (1-t) * t * t * t * 255);
-	g = (int)(2. * (1-t) * (1-t) * t * t * 255);
-	b = (int)(5 * (1-t) * (1-t) * (1-t) * t * 255);
-	color = (r << 16) | (g << 8) | b;
-	return (color);
+
+	// curva lenta, sensação de sonho
+	t = pow(t, 0.6);
+
+	r = (int)(sin(t * 4.0 + 0.0) * 30 + 200);
+	g = (int)(sin(t * 4.0 + 2.0) * 30 + 195);
+	b = (int)(sin(t * 4.0 + 4.0) * 40 + 215);
+
+	return ((r << 16) | (g << 8) | b);
 }
--- src/parser.c
+++ src/parser.c
@@ -6,11 +6,34 @@
 /*   By: osousa-d <osousa-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 11:25:25 by osousa-d          #+#    #+#             */
-/*   Updated: 2026/01/27 11:38:11 by osousa-d         ###   ########.fr       */
+/*   Updated: 2026/01/27 12:38:03 by osousa-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
+
+int	is_num(char *str)
+{
+	int	i;
+	int	has_dot;
+
+	i = 0;
+	has_dot = 0;
+	if (ft_issignal(str[i]))
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+		{
+			if (str[i] == '.' && has_dot == 0)
+				has_dot = 1;
+			else
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
 
 int parse_args(int ac, char **av, t_fractal *f)
 {
@@ -21,6 +44,8 @@ int parse_args(int ac, char **av, t_fractal *f)
 	}
 	if (ac == 4 && ft_strcmp(av[1], "julia") == 0)
 	{
+		if (!is_num(av[2]) || !is_num(av[3]))
+			return (0);
 		data_init(f, "Julia");
 		f->julia_v.real = ft_atod(av[2]);
 		f->julia_v.imag = ft_atod(av[3]);   
